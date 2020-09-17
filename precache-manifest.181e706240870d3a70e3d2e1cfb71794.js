14ec3a06f16c6b31783c5fa5609c0316a3aa43b0
Moostanger <lambo-555@yandex.ru>
2020-09-18 02:50:33 +0300

    Add files via upload

---
+++ precache-manifest.181e706240870d3a70e3d2e1cfb71794.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bba0174c7f15a4ac7ef3c164e0fc38e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "51e39031d1f108d52ab6",
+    "url": "./static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "4b127015db4792f9be9a",
+    "url": "./static/css/main.e3918e0e.chunk.css"
+  },
+  {
+    "revision": "51e39031d1f108d52ab6",
+    "url": "./static/js/2.61e76111.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.61e76111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b127015db4792f9be9a",
+    "url": "./static/js/main.7f151702.chunk.js"
+  },
+  {
+    "revision": "e8aa6809182200030008",
+    "url": "./static/js/runtime-main.622226ce.js"
+  }
+]);
